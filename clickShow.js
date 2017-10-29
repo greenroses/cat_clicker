e1faa58a979c87b0f5c76ae633aa456dd6079147
greenroses <xiaodan.zju@gmail.com>
2017-10-29 18:53:53 -0400

    feature: Add reference of images

--- clickShow.js
+++ clickShow.js
@@ -4,31 +4,36 @@ window.onload=function(){
         name: 'Kitty',
         image: 'cat_1.png',
         num: 0,
-        show: false
+        show: false,
+        ref: 'https://www.flickr.com/photos/poplinre/625069434/in/photostream/'
     },
     {
         name: 'Mitten',
         image: 'cat_2.png',
         num: 0,
-        show: false
+        show: false,
+        ref: 'https://www.flickr.com/photos/chewie/2290467335'
     },
     {
         name: 'Max',
         image: 'cat_3.png',
         num: 0,
-        show: false
+        show: false,
+        ref: 'https://www.flickr.com/photos/23435342@N05/2241767254/'
     },
     {
         name: 'Cindy',
         image: 'cat_4.png',
         num: 0,
-        show: false
+        show: false,
+        ref: 'https://www.flickr.com/photos/brothersoft/3828283021/'
     },
     {
         name: 'Peanut',
         image: 'cat_5.png',
         num: 0,
-        show: false
+        show: false,
+        ref: 'https://www.flickr.com/photos/newstock/3867418282/'
     },
     ]
 
@@ -51,6 +56,8 @@ window.onload=function(){
             elem.appendChild(document.createElement('br'));
             elem.appendChild(img);
             img.src = cat.image;
+            elem.appendChild(document.createElement('br'));
+            elem.appendChild(document.createTextNode(cat.ref));
             document.body.appendChild(elem);
 
 
